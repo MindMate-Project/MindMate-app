e9db37e1b126f748c16825355f2d38d95086c84e
Omnia Samir <omnia.sa19@gmail.com>
2026-02-02 08:01:07 +0200

    onboarding

---
+++ lib/screens/onboarding/patient/ui/patient_onboarding_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import '../data/patient_onboarding_data.dart';
+import 'package:mindmate/screens/onboarding/common/onboarding_page.dart';
+
+class PatientOnboardingScreen extends StatefulWidget {
+  const PatientOnboardingScreen({super.key});
+
+  @override
+  State<PatientOnboardingScreen> createState() =>
+      _PatientOnboardingScreenState();
+}
+
+class _PatientOnboardingScreenState extends State<PatientOnboardingScreen> {
+  final PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  void _nextPage() {
+    if (_currentIndex < patientOnboardingData.length - 1) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      _goToAuth();
+    }
+  }
+
+  void _previousPage() {
+    if (_currentIndex > 0) {
+      _pageController.previousPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  void _skipOnboarding() {
+    _goToAuth();
+  }
+
+  void _goToAuth() {
+    Navigator.pushReplacementNamed(context, '/signup');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isLastPage = _currentIndex == patientOnboardingData.length - 1;
+
+    return Scaffold(
+      body: SafeArea(
+        minimum: const EdgeInsets.symmetric(horizontal: 37, vertical: 40),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              // Skip button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'assets/images/splash.png',
+                    width: 35,
+                    height: 31,
+                  ),
+                  TextButton(
+                    onPressed: _skipOnboarding,
+                    child: const Text('Skip'),
+                  ),
+                ],
+              ),
+              // PageView
+              Expanded(
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: patientOnboardingData.length,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  },
+                  itemBuilder: (context, index) {
+                    return OnboardingPage(item: patientOnboardingData[index]);
+                  },
+                ),
+              ),
+
+              // Buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  if (_currentIndex > 0)
+                    TextButton(
+                      onPressed: _previousPage,
+                      child: const Text(
+                        'Back',
+                        style: TextStyle(
+                          color: Color(0xff5EA5C0),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                    )
+                  else
+                    const SizedBox(width: 60),
+
+                  IconButton(
+                    onPressed: _nextPage,
+                    icon: Icon(
+                      // isLastPage
+                      // ? Icons.check_circle_outline
+                      Icons.arrow_forward_ios_rounded,
+                      color: const Color(0xff5EA5C0),
+                      size: 30,
+                    ),
+                  ),
+                  // ElevatedButton(
+                  //   onPressed: _nextPage,
+                  //   child: Text(isLastPage ? 'Get Started' : 'Next'),
+                  // ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
