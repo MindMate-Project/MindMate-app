b14df00961e4e4a148a5ce6671daefd484ec7aec
Omnia Samir <omnia.sa19@gmail.com>
2026-02-20 13:14:57 +0200

    add responsive file

--- lib/main.dart
+++ lib/main.dart
@@ -13,45 +13,53 @@ import 'package:mindmate/screens/auth/updated_pass.dart';
 import 'package:mindmate/screens/onboarding/common/role_selection_page.dart';
 import 'package:mindmate/screens/onboarding/common/onboarding_screen.dart';
 import 'package:mindmate/screens/onboarding/common/onboarding_item.dart';
+import 'package:mindmate/screens/reminders/reminders.dart';
+import 'package:mindmate/utils/responsive.dart';
 
 void main() {
   runApp(
     BlocProvider(
       create: (context) => AuthCubit(AuthService()),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/splash',
-        routes: {
-          '/splash': (context) => Splash(),
-          '/roleSelection': (context) => RoleSelectionPage(),
-          '/login': (context) => Login(),
-          '/signup': (context) => Signup(),
-          '/home': (context) => HomePage(),
-          '/forgot_password': (context) => ForgotPasswordScreen(),
-          '/updatedpass': (context) => UpdatedPass(),
-        },
-        onGenerateRoute: (settings) {
-          if (settings.name == '/onboarding') {
-            final items = settings.arguments as List<OnboardingItem>;
-            return MaterialPageRoute(
-              builder: (_) => OnboardingScreen(items: items),
-            );
-          }
-          if (settings.name == '/verify-code') {
-            final email = settings.arguments as String;
-            return MaterialPageRoute(
-              builder: (context) => VerifyCodeScreen(email: email),
-            );
-          }
-          if (settings.name == '/reset-password') {
-            final email = settings.arguments is String
-                ? settings.arguments as String
-                : (settings.arguments as Map<String, String>?)?['email'];
-            return MaterialPageRoute(
-              builder: (context) => ResetPasswordScreen(email: email),
-            );
-          }
-          return null;
+      child: Builder(
+        builder: (context) {
+          Responsive.init(context);
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/splash',
+            routes: {
+              '/splash': (context) => Splash(),
+              '/roleSelection': (context) => RoleSelectionPage(),
+              '/login': (context) => Login(),
+              '/signup': (context) => Signup(),
+              '/home': (context) => HomePage(),
+              '/forgot_password': (context) => ForgotPasswordScreen(),
+              '/updatedpass': (context) => UpdatedPass(),
+              '/reminders': (context) => RemindersScreen(),
+            },
+            onGenerateRoute: (settings) {
+              if (settings.name == '/onboarding') {
+                final items = settings.arguments as List<OnboardingItem>;
+                return MaterialPageRoute(
+                  builder: (_) => OnboardingScreen(items: items),
+                );
+              }
+              if (settings.name == '/verify-code') {
+                final email = settings.arguments as String;
+                return MaterialPageRoute(
+                  builder: (context) => VerifyCodeScreen(email: email),
+                );
+              }
+              if (settings.name == '/reset-password') {
+                final email = settings.arguments is String
+                    ? settings.arguments as String
+                    : (settings.arguments as Map<String, String>?)?['email'];
+                return MaterialPageRoute(
+                  builder: (context) => ResetPasswordScreen(email: email),
+                );
+              }
+              return null;
+            },
+          );
         },
       ),
     ),
