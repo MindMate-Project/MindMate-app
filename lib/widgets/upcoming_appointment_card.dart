78af774ee040f9e0800a53535a9e3601b430bd6e
Omnia Samir <omnia.sa19@gmail.com>
2026-02-20 13:30:23 +0200

    build appointmentCard as custom widget

---
+++ lib/widgets/upcoming_appointment_card.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:mindmate/themes/app_theme.dart';
+
+class UpcomingAppointmentCard extends StatelessWidget {
+  final String doctorName;
+  final String specialty;
+  final String date;
+  final String time;
+  final String? imageUrl;
+
+  const UpcomingAppointmentCard({
+    Key? key,
+    required this.doctorName,
+    required this.specialty,
+    required this.date,
+    required this.time,
+    this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: AppTheme.neutralLight,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              // Doctor's profile picture
+              Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppTheme.neutralWhite,
+                  image: imageUrl != null
+                      ? DecorationImage(
+                          image: NetworkImage(imageUrl!),
+                          fit: BoxFit.cover,
+                        )
+                      : null,
+                ),
+                child: imageUrl == null
+                    ? Icon(Icons.person, size: 32, color: AppTheme.primaryColor)
+                    : null,
+              ),
+              const SizedBox(width: 15),
+              // Doctor's details
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      doctorName,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF2D3142),
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      specialty,
+                      style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          // Appointment date and time
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        spacing: 6,
+                        children: [
+                          Icon(
+                            Icons.calendar_today,
+                            color: Color(0xffADADAD),
+                            size: 20,
+                          ),
+                          Text(
+                            date,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppTheme.neutralDark,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        spacing: 6,
+                        children: [
+                          Icon(
+                            Icons.access_time,
+                            color: Color(0xffADADAD),
+                            size: 20,
+                          ),
+                          Text(
+                            time,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppTheme.neutralDark,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
