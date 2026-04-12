cb59c9194ff1338bf74bb77d6dcab371285d2a25
Omnia Samir <omnia.sa19@gmail.com>
2026-04-12 04:31:23 +0200

    Add custom elevated button and patient card widget; update info pill card and profile app bar layout

--- lib/core/widgets/info_pill_card.dart
+++ lib/core/widgets/info_pill_card.dart
@@ -16,7 +16,7 @@ class InfoPillCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10),
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
       decoration: BoxDecoration(
         color: AppTheme.neutralLight,
         borderRadius: BorderRadius.circular(16),
@@ -32,7 +32,7 @@ class InfoPillCard extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: children,
-              ), 
+              ),
             ),
           ),
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mindmate/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:mindmate/features/patient/home/presentation/screens/home_screen.dart';
-import 'package:mindmate/features/caregiver/presentation/screens/home_screen.dart'
+import 'package:mindmate/features/caregiver/home/presentation/screens/caregiver_home_screen.dart'
     as caregiver;
 import 'package:mindmate/features/auth/data/services/auth_service.dart';
 import 'package:mindmate/features/auth/presentation/screens/splash_screen.dart';
@@ -25,14 +25,11 @@ import 'package:mindmate/core/utils/responsive.dart';
 import 'package:mindmate/features/memory/presentation/screens/memory_screen.dart';
 import 'package:mindmate/features/memory/presentation/cubit/memory_cubit.dart';
 import 'package:mindmate/features/memory/data/services/memory_service.dart';
-<<<<<<< HEAD
-=======
 import 'package:mindmate/features/patient/profile/presentation/cubit/profile_cubit.dart';
 import 'package:mindmate/features/patient/profile/data/services/profile_service.dart';
 import 'package:mindmate/features/patient/reminders/presentation/cubit/reminders_cubit.dart';
 import 'package:mindmate/features/patient/reminders/data/services/reminders_service.dart';
 
->>>>>>> 91d6612e0fe258ee1fa14b3550cd5338d400da2e
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
